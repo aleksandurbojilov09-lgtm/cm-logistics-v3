e4f45dd6fe19900993f74a6ee01efd6e1390406e
Aleksandur Bozhilov <aleksandurbojilov09@gmail.com>
2026-08-14 13:05:30 +0300

    Refactor main.ts to start router and remove login page

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,16 +1,7 @@
 import "./styles.css";
 
 import {
-    initializeLoginPage,
-    renderLoginPage
-} from "../pages/login/login-page";
+    startRouter
+} from "./router";
 
-const app = document.querySelector<HTMLDivElement>("#app");
-
-if (!app) {
-    throw new Error("K3 Logistics: #app root element was not found.");
-}
-
-app.innerHTML = renderLoginPage();
-
-initializeLoginPage();
+startRouter();
